8f7490312c0beee0977dd586bd3ab6db34ed48b6
Remees Salam <remeessalam@Remeess-MacBook-Air.local>
2023-01-28 12:40:10 +0530

    react from scratch is working

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <>
+        <h1>
+            Hello world! I am using React hai
+        </h1>
+        <button>ha</button>
+        </>
+    )
+}
+
+export default App
